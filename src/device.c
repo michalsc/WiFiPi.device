3aea69a87b96289ebb27541f6d08763a035548d4
Michal Schulz <michal.schulz@gmx.de>
2024-04-01 15:38:31 +0000

    Fetch for larger buffer copy functions

--- src/device.c
+++ src/device.c
@@ -99,11 +99,14 @@ static BPTR WiFi_Expunge(REGARG(struct WiFiBase * WiFiBase, "a6"))
 
 static const ULONG rx_tags[] = {
     S2_CopyToBuff,
+    S2_CopyToBuff16,
+    S2_CopyToBuff32,
     0
 };
 
 static const ULONG tx_tags[] = {
     S2_CopyFromBuff,
+    S2_CopyFromBuff16,
     0
 };
 
